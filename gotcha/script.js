06204885b5a2975c548715dfd28d4be5687cb791
arem <andrewvillalon123@gmail.com>
2022-08-24 03:01:05 +0800

    update 3am

--- gotcha/script.js
+++ gotcha/script.js
@@ -19,5 +19,13 @@
     let newWindow;
 
     openBtn.addEventListener('click', function() {
-        newWindow = window.open('https://meraeugene.github.io/login/', 'newWindow', 'width=600, height=700, left=300, top=200 ')
+        newWindow = window.open('https://meraeugene.github.io/login/', 'newWindow', 'width=1100, height=700, left=100, top=200 ')
+    })
+
+    const openBtntwo = document.getElementById('openTwo');
+
+    let newWindowtwo;
+
+    openBtntwo.addEventListener('click', function() {
+        newWindowtwo = window.open('https://meraeugene.github.io/signup/', 'newWindow', 'width=1100, height=700, left=100, top=200 ')
     })
